4fd9d045e5b45d4c0144f77049c0bf525c58ae90
Henrique <venoshk.henrique19@gmail.com>
2024-06-26 15:48:37 -0300

    Deixando a o codigo limpo

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,94 +1,104 @@
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import { useForm } from 'react-hook-form';
 import { toast, Bounce } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Context } from "../context/Provider";
 import Loading from "../Loading/Loading";
 import { CiLogout } from "react-icons/ci";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+// Função para exibir notificações de sucesso e erro
+const notify = (message, type) => {
+  const options = {
+    position: "top-left",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: false,
+    progress: undefined,
+    theme: "light",
+    transition: Bounce,
+  };
+
+  if (type === 'success') {
+    toast.success(message, options);
+  } else {
+    toast.error(message, options);
+  }
+};
+
+// Função para formatar número de telefone
+const handlePhoneChange = (e) => {
+  const phone = e.target.value.replace(/\D/g, '');
+  if (phone.length > 12) {
+    e.target.value = phone.slice(0, 11);
+  }
+};
+
+// Função para buscar endereço pelo CEP
+const handleCepChange = async (e, setAddress, notify) => {
+  const cep = e.target.value.replace(/\D/g, '');
+  if (cep.length > 8) {
+    e.target.value = cep.slice(0, 8);
+    return;
+  }
+  if (cep.length === 8) {
+    try {
+      const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      const data = await response.json();
+      if (data.erro) {
+        notify('Cep inválido!', 'error');
+        setAddress("");
+      } else {
+        setAddress(data.logradouro, data.uf);
+      }
+    } catch (error) {
+      notify('Cep inválido!', 'error');
+      setAddress("");
+    }
+  } else {
+    setAddress("");
+  }
+};
+
+const InputField = ({ register, name, placeholder, type, errors, validation, onChange }) => (
+  <>
+    <input
+      type={type}
+      {...register(name, validation)}
+      placeholder={placeholder}
+      className="bg-white-100 px-4 py-4 outline-none placeholder:font-bold border border-black/35 rounded focus:border-green-500"
+      onChange={onChange}
+    />
+    {errors[name] && <span className="text-red-600">{errors[name].message}</span>}
+  </>
+);
 
 export const Form = () => {
   const { register, handleSubmit, formState: { errors }, reset } = useForm();
   const [address, setAddress] = useState("");
-  const { cartItems, loading, setLoading, isOpen } = useContext(Context);
-
-  const notifySuccess = () => toast.success('Pedido enviado!', {
-    position: "top-left",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: false,
-    draggable: false,
-    progress: undefined,
-    theme: "light",
-    transition: Bounce,
-  });
-
-  const notifyError = (message) => toast.error(message, {
-    position: "top-left",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: false,
-    draggable: false,
-    progress: undefined,
-    theme: "light",
-    transition: Bounce,
-  });
-
-  const handlePhoneChange = (e) => {
-    const phone = e.target.value.replace(/\D/g, '');
-
-    if(phone.length > 12){
-      e.target.value = phone.slice(0,11);
-      return;
-    }
-  }
-
-  const handleCepChange = async (e) => {
-    const cep = e.target.value.replace(/\D/g, ''); // Remove qualquer caractere não numérico
-    if (cep.length > 8) {
-      e.target.value = cep.slice(0, 8);
-      return;
-    }
-    if (cep.length === 8) {
-      try {
-        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        const data = await response.json();
-        if (data.erro) {
-          notifyError('Cep inválido!');
-          setAddress("");
-        } else {
-          setAddress(data.logradouro, data.uf);
-        }
-      } catch (error) {
-        notifyError('Cep invalido!');
-        setAddress("");
-      }
-    } else {
-      setAddress("");
-    }
-  };
+  const { cartItems, setCartItems , loading, setLoading, isOpen, } = useContext(Context);
+  const navigate = useNavigate();
 
   const onSubmit = async (data) => {
-    if(!isOpen){
-      notifyError('Ops! O restaurante está fechado');
+    if (!isOpen) {
+      notify('Ops! O restaurante está fechado', 'error');
       return;
     }
 
     if (cartItems.length === 0) {
-      notifyError("Carrinho está vazio!");
+      notify("Carrinho está vazio!", 'error');
       return;
     }
 
-    const total = cartItems.reduce((acc, item) =>  acc + (item.price * item.qualify),0);
-
+    const total = cartItems.reduce((acc, item) => acc + (item.price * item.qualify), 0);
     const pedidoData = {
       name: data.name,
       message: data.message,
       home: data.home,
       address: address,
-      total: total, 
+      total: total,
       cart: cartItems,
       number: data.phone
     };
@@ -104,14 +114,15 @@ export const Form = () => {
       });
 
       if (response.ok) {
-        notifySuccess();
+        notify('Pedido enviado!', 'success');
         reset();
-        cartItems.length = 0
+        setCartItems([]); // Limpa o carrinho de compras
+        navigate('/'); // Redireciona para a página inicial
       } else {
-        notifyError('Erro ao enviar pedido');
+        notify('Erro ao enviar pedido', 'error');
       }
     } catch (error) {
-      notifyError('Ops algo não deu certo!');
+      notify('Ops algo não deu certo!', 'error');
     } finally {
       setLoading(false);
     }
@@ -119,44 +130,49 @@ export const Form = () => {
 
   return (
     <>
-      {loading ? (<Loading/>) : (
+      {loading ? (<Loading />) : (
         <div className="w-full">
           <div className="bg-white w-full text-red-600 py-2 px-2 text-4xl">
-           <Link to={'/'}>
-              <CiLogout/>
-           </Link>
+            <Link to={'/'}>
+              <CiLogout />
+            </Link>
           </div>
           <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-full gap-4 mt-5 mb-1 px-4">
             <h1 className="text-center text-3xl font-bold">Enviar seu <span className="text-red-600">pedido</span></h1>
-            <input
-              {...register("name", { required: true })}
+            <InputField
+              register={register}
+              name="name"
               placeholder="Nome"
-              className="bg-white-100 px-4 py-4 outline-none placeholder:font-bold border border-black/35 rounded focus:border-green-500"
+              type="text"
+              errors={errors}
+              validation={{ required: "O nome não pode estar vazio" }}
             />
-            {errors.name && <span className="text-red-600">O nome não pode estar vazio</span>}
-            <input
-              type="tel"
-              {...register("phone", { required: true, pattern: /^[0-9]{11}$/ })}
+            <InputField
+              register={register}
+              name="phone"
               placeholder="Telefone"
-              className="bg-white-100 px-4 py-4 outline-none placeholder:font-bold border border-black/35 rounded focus:border-green-500"
+              type="number"
+              errors={errors}
+              validation={{ required: "O número é inválido!", pattern: /^[0-9]{11}$/ }}
               onChange={handlePhoneChange}
             />
-            {errors.phone && <span className="text-red-600">O número é inválido!</span>}
-            <input
-              type="email"
-              {...register("email", { required: true })}
+            <InputField
+              register={register}
+              name="email"
               placeholder="Email"
-              className="bg-white-200 px-4 py-4 outline-none placeholder:font-bold border border-black/35 rounded focus:border-green-500"
+              type="email"
+              errors={errors}
+              validation={{ required: "Email inválido!" }}
             />
-            {errors.email && <span className="text-red-600">Email inválido!</span>}
-            <input
-              type="number"
-              {...register("cep", { required: true, pattern: /^[0-9]{8}$/ })}
+            <InputField
+              register={register}
+              name="cep"
               placeholder="Cep"
-              className="bg-white-200 px-4 py-4 outline-none placeholder:font-bold border border-black/35 rounded focus:border-green-500"
-              onChange={handleCepChange}
+              type="number"
+              errors={errors}
+              validation={{ required: "Cep inválido", pattern: /^[0-9]{8}$/ }}
+              onChange={(e) => handleCepChange(e, setAddress, notify)}
             />
-            {errors.cep && <span className="text-red-600">Cep inválido</span>}
             <input
               type="text"
               value={address}
@@ -164,13 +180,14 @@ export const Form = () => {
               className="bg-white-200 px-4 py-4 outline-none placeholder:font-bold border border-black/35 rounded focus:border-green-500"
               readOnly
             />
-            <input
-              type="text"
-              {...register("home", { required: true })}
+            <InputField
+              register={register}
+              name="home"
               placeholder="Casa/Apartamento"
-              className="bg-white-100 px-4 py-4 outline-none placeholder:font-bold border border-black/35 rounded focus:border-green-500"
+              type="text"
+              errors={errors}
+              validation={{ required: "Este campo é obrigatório" }}
             />
-            {errors.home && <span className="text-red-600">Este campo é obrigatório</span>}
             <textarea
               {...register("message")}
               placeholder="Mensagem (opcional)"
@@ -179,9 +196,7 @@ export const Form = () => {
             <button className="border py-3 bg-green-500 text-white font-bold text-2xl">Fazer pedido</button>
           </form>
         </div>
-           
       )}
     </>
-
   );
-}
+};
--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,14 +3,14 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import "./Loading.css"
 function Loading(){
     return(
-        <div className="relative animate-pulse gap-2 p-4 mb-10 w-full flex items-center">
-            <div className="h-12 w-12 rounded-full bg-slate-400"></div>
-            <div className="flex-1">
-                <div className="mb-1 h-5 w-3/5 rounded-lg bg-slate-400 text-lg"></div>
-                <div className="h-5 w-[90%] rounded-lg bg-slate-400 text-sm"></div>
-            </div>
-            <div className="absolute bottom-5 right-0 h-4 w-4 rounded-full bg-slate-400"></div>
+        <div className="loader text-center flex flex-col items-center justify-center">
+            <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-red-600 mx-auto"></div>
+            <h2 className="text-zinc-900 dark:text-white mt-4">Enviando...</h2>
+            <p className="text-zinc-600 dark:text-zinc-400">
+                Enviando seu pedido!
+            </p>
         </div>
+
     )
 };
 
