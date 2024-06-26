8d6c4838c4c9e08492bff826eb8051e618fd5d67
Henrique <venoshk.henrique19@gmail.com>
2024-06-26 00:27:34 -0300

    Mudando a api

--- src/api/fetchProducts.js
+++ src/api/fetchProducts.js
@@ -1,6 +1,6 @@
 export const fetchProducts = async (type) => {
     const query = type ? `?type=${type}` : '';
-    const response = await fetch(`https://backend-pi-virid.vercel.app/${query}`);
+    const response = await fetch(`https://muchies-backend.vercel.app/${query}`);
     const data = await response.json();
     return data;
 };
